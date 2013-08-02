07b2e1d853f1e6fcb6ef7d21dca31a8742db9bd0
evertonsilva <thonns@gmail.com>
2013-08-02 14:30:48 -0300

    hw3 part one done

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,12 +4,12 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-  end
-  flunk "Unimplemented"
+    Movie.create(movie)
+  end  
 end
 
 # Make sure that one string (regexp) occurs before or after another one
-#   on the same page
+# on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
